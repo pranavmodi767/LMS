319c57dc936869e5f4a04402715c6f885fe7c70b
Pranav <pranavmodi767@gmail.com>
2021-05-27 17:14:37 +0530

    v1.0.6-Added Job messaging

---
+++ app/src/main/java/com/example/labour/bat.java
@@ -0,0 +1,17 @@
+package com.example.labour;
+
+public class bat {
+
+    public String batMobile,batMessage;
+
+
+    public String getBatMobile(){return  batMobile;}
+    public void setBatMobile(String batMobile){this.batMobile=batMobile;}
+
+
+    public String getBatMessage(){return batMessage;}
+    public void setBatMessage(String batMessage){this.batMessage=batMessage;}
+
+
+}
+
--- app/src/main/java/com/example/labour/createJob.java
+++ app/src/main/java/com/example/labour/createJob.java
@@ -2,9 +2,14 @@ package com.example.labour;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -27,7 +32,7 @@ import java.util.ArrayList;
 
 public class createJob extends AppCompatActivity {
 
-    private EditText jobName,jobDisription,jobLocation,jobPay;
+    private EditText jobName, jobDisription, jobLocation, jobPay;
     private EditText jobRequirements;
     private Button jobBtn;
     private Spinner assign;
@@ -43,47 +48,49 @@ public class createJob extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_job);
 
-        mAuth =FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-        assign=findViewById(R.id.assign);
+        assign = findViewById(R.id.assign);
         jobName = findViewById(R.id.jobName);
         jobDisription = findViewById(R.id.jobDiscription);
-        jobLocation =findViewById(R.id.jobLocation);
-        jobPay =findViewById(R.id.jobPay);
+        jobLocation = findViewById(R.id.jobLocation);
+        jobPay = findViewById(R.id.jobPay);
         jobRequirements = findViewById(R.id.jobRequirements);
 
-        pp =findViewById(R.id.pp);
+        pp = findViewById(R.id.pp);
 
         pp.setVisibility(View.VISIBLE);
         ArrayList<String> list = new ArrayList<>();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, list);
         assign.setAdapter(adapter);
 
-        database=FirebaseDatabase.getInstance();
-        myRef=database.getReference().child("Workers");
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference().child("Workers");
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                for (DataSnapshot snappy : snapshot.getChildren()){
+                for (DataSnapshot snappy : snapshot.getChildren()) {
                     //list.clear();
                     list.add(snappy.getValue().toString());
                 }
                 adapter.notifyDataSetChanged();
                 pp.setVisibility(View.GONE);
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
         //pp.setVisibility(View.GONE);
-        jobBtn =findViewById(R.id.jobBtn);
+        jobBtn = findViewById(R.id.jobBtn);
         jobBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 job();
+
             }
 
 
@@ -93,96 +100,129 @@ public class createJob extends AppCompatActivity {
     }
 
 
-    public void job(){
-        String JobName =jobName.getText().toString().trim();
-        String JobDiscription=jobDisription.getText().toString().trim();
+    public void job() {
+        String JobName = jobName.getText().toString().trim();
+        String JobDiscription = jobDisription.getText().toString().trim();
         String JobLocation = jobLocation.getText().toString().trim();
         String JobPay = jobPay.getText().toString().trim();
-        String JobRequirements=jobRequirements.getText().toString().trim();
+        String JobRequirements = jobRequirements.getText().toString().trim();
 
-        String Assign =assign.getSelectedItem().toString().trim();
+        String Assign = assign.getSelectedItem().toString().trim();
 
         database = FirebaseDatabase.getInstance();
 
         SharedPreferences countSettings = getSharedPreferences("count", 0);
         // get current counts
-        int count = countSettings.getInt("counts",0);
+        int count = countSettings.getInt("counts", 0);
         count++;
         final SharedPreferences.Editor edit = countSettings.edit();
-        edit.putInt("counts",count);
+        edit.putInt("counts", count);
         edit.commit();
 
-        Job hob = new Job(Assign,JobName,JobDiscription,JobLocation,JobPay,JobRequirements);
+        Job hob = new Job(Assign, JobName, JobDiscription, JobLocation, JobPay, JobRequirements);
 
-        myRef =database.getReference().child("Jobs").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(String.valueOf(count));
+        myRef = database.getReference().child("Jobs").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(String.valueOf(count));
         myRef.setValue(hob).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
-                   Toast.makeText(createJob.this, "Job Created Successfully!", Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(createJob.this,"Failed to Create Job!",Toast.LENGTH_LONG).show();
+                    Toast.makeText(createJob.this, "Job Created Successfully!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(createJob.this, "Failed to Create Job!", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
-        getMob(Assign,JobName,JobDiscription,JobLocation,JobPay,JobRequirements);
+        getMob(Assign, JobName, JobDiscription, JobLocation, JobPay, JobRequirements);
     }
 
     private void getMob(String assign, String jobName, String jobDiscription, String jobLocation, String jobPay, String jobRequirements) {
 
 
-        String[] str =assign.split(":",0);
-       Toast.makeText(createJob.this,str[0],Toast.LENGTH_SHORT).show();
+        String[] str = assign.split(":", 0);
+        Toast.makeText(createJob.this, str[0], Toast.LENGTH_SHORT).show();
 
-        database =FirebaseDatabase.getInstance();
-        myRef =database.getReference().child("Users");
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference().child("Users");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                for(DataSnapshot snap : snapshot.getChildren()){
+                for (DataSnapshot snap : snapshot.getChildren()) {
                     Log.d("name", String.valueOf(snap.child("name")));
-                    if(snap.child("name").getValue().equals(str[0].trim())){
-                        Log.d("snap","Found");
-                        String mob=snap.child("mobile").getValue().toString().trim();
+                    if (snap.child("name").getValue().equals(str[0].trim())) {
+                        Log.d("snap", "Found");
+                        String mob = snap.child("mobile").getValue().toString().trim();
                         //Toast.makeText(createJob.this,mob,Toast.LENGTH_SHORT).show();
-                        sendMsg(mob,jobName,jobDiscription,jobLocation,jobPay,jobRequirements);
-                    }else {
-                        Log.d("snap","Not Found");
+                        sendMsg(mob, jobName, jobDiscription, jobLocation, jobPay, jobRequirements);
+                    } else {
+                        Log.d("snap", "Not Found");
                     }
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
     }
 
-    private void sendMsg(String mob,String jobName, String jobDiscription, String jobLocation, String jobPay,String jobRequirements) {
+    private void sendMsg(String mob, String jobName, String jobDiscription, String jobLocation, String jobPay, String jobRequirements) {
 
         //Toast.makeText(createJob.this,String.valueOf(mob),Toast.LENGTH_SHORT).show();
-        Log.d("details",jobName);
-        Log.d("details",jobDiscription);
-        Log.d("details",jobLocation);
-        Log.d("details",jobPay);
-        Log.d("details",jobRequirements);
-        Log.d("details",String.valueOf(mob));
+        Log.d("details", jobName);
+        Log.d("details", jobDiscription);
+        Log.d("details", jobLocation);
+        Log.d("details", jobPay);
+        Log.d("details", jobRequirements);
+        Log.d("details", String.valueOf(mob));
 
         StringBuilder sb = new StringBuilder();
-        sb.append("Subject: "+jobName+
-                "\nDiscription: "+jobDiscription+
-                "\nLocation: "+jobLocation+
-                "\nPayment: "+jobPay+
-                "\nRequirements:"+jobRequirements);
+        sb.append("Subject: " + jobName +
+                "\nDiscription: " + jobDiscription +
+                "\nLocation: " + jobLocation +
+                "\nPayment: " + jobPay +
+                "\nRequirements:" + jobRequirements);
 
         String batMobile = mob;
-        String batMessage =sb.toString();
+        String batMessage = sb.toString();
 
-        Log.d("bat",batMobile);
-        Log.d("bat",batMessage);
+        Log.d("bat", batMobile);
+        Log.d("bat", batMessage);
         //Send batMessage String to the batMobile
 
+        if(ContextCompat.checkSelfPermission(createJob.this,
+                Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
 
+                bat china = new bat();
+                china.setBatMobile(batMobile);
+                china.setBatMessage(batMessage);
+                batSignal(batMobile,batMessage);
+
+        }else {
+            ActivityCompat.requestPermissions(createJob.this,new String[]{Manifest.permission.SEND_SMS}
+            ,100);
+        }
+    }
+
+    private void batSignal(String batMobile, String batMessage) {
+        SmsManager simp =SmsManager.getDefault();
+        simp.sendTextMessage(batMobile,null,batMessage,null,null);
+        Toast.makeText(createJob.this,"message Sent",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions, @NonNull  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode ==100 && grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            bat b = new bat();
+
+            batSignal(b.getBatMobile(),b.getBatMessage());
+        }else {
+            Toast.makeText(createJob.this,"Permission Denied",Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
